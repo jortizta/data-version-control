9d6e26bf2d663465ca753c7923f8813d7417e36c
Jose Luis Ortiz-Tarin <JoseLuis.Ortiz-Tarin@MBP-JoseLuis.Ortiz-Tarin>
2022-01-11 15:58:26 +0100

    Created train and test CSV files

--- src/prepare.py
+++ src/prepare.py
@@ -36,5 +36,6 @@ def main(repo_path):
 
 
 if __name__ == "__main__":
-    repo_path = Path(__file__).parent.parent
+    repo_path = Path(__file__).resolve().parent.parent
+    #print(repo_path)
     main(repo_path)
